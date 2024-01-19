1b00cdf0edd08c3833826b5e66fa75619aa8e197
WeronikaGRIG <veronikaudot@gmail.com>
2024-01-19 16:34:09 +0100

    new change

--- script.js
+++ script.js
@@ -3,33 +3,35 @@ const input = document.querySelector('input');
 const answer = document.querySelector('.answer');
 const error = document.querySelector('.error');
 
-const answerArr = ['Tak!', 'Nie', 'Może...', 'Ciężko powiedzieć...', 'Nie chcę znać odpowiedz na to pytanie!'];
+const answersArr = ['Tak!', 'Nie.', 'Może.', 'Ciężko powiedzieć...', 'Nie chcesz znać odpowiedzi na to pytanie... :/'];
+
 
 const shakeBall = () => {
-    ball.classList.add('shake-animation')
+
+    ball.classList.add('shake-animaton');
     setTimeout(checkInput, 1000)
 }
 
 const checkInput = () => {
     if (input.value !== '' && input.value.slice(-1) === '?') {
         generateAnswer();
-        error.textContent = ''
-        ball.classList.remove('shake-animation')
+        error.textContent = '';
     } else if (input.value !== '' && input.value.slice(-1) !== '?') {
-        error.textContent = 'Pytanie musi byc zakończone znakiem "?".'
+        error.textContent = 'Pytanie musi być zakończone znakiem "?".';
         answer.textContent = ''
-        ball.classList.remove('shake-animation')
     } else {
-        error.textContent = 'Musisz zadać jakieś pytanie!'
+        error.textContent = 'Musisz zadać jakieś pytanie!';
         answer.textContent = ''
-        ball.classList.remove('shake-animation')
     }
+
+    ball.classList.remove('shake-animaton');
 }
+
 
 const generateAnswer = () => {
-
-    const number = Math.floor(Math.random() * 5)
-    answer.innerHTML = `<span>Odpowidz:</span> ${answerArr[number]}`
+    const number = Math.floor(Math.random() * 5);
+    answer.innerHTML = `<span>Odpowiedź:</span> ${answersArr[number]}`
 }
 
-ball.addEventListener('click', generateAnswer);
+
+ball.addEventListener('click', shakeBall)
